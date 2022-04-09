3d5dc74956f931b564745a293c7c0bc9029e2152
Wesley Nilton <90294657+Wesley-Nilton@users.noreply.github.com>
2022-04-09 17:46:00 -0300

    Create class 14

---
+++ aula14/ambiente.js
@@ -0,0 +1,15 @@
+/*
+var c = 1;
+do{
+    console.log(c);
+    c++;
+} while(c <= 10);
+*/
+
+// -----------------------
+
+console.log("Vai começar...");
+for(c = 1; c <= 10; c++){
+    console.log(c);
+}
+console.log("FIM!");
